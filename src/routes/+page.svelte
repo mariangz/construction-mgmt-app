e8cdb05baeff1c3f8b146fe6c54cc9fb1ad28956
Mariano <marianosgimenez@gmail.com>
2025-06-20 17:58:58 +0200

    feat: add main page and sync with couchdb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+	import { taskDatabase } from '$lib/db';
+
+  const remoteUrl = 'http://admin:admin@localhost:5984/construction-app';
+  onMount(() => {
+		taskDatabase.liveSync(remoteUrl);
+	});
+</script>
+
 <main class="container">
-  <h1>Construction App</h1>
-  <nav>
-    <ul>
-      <li><a href="/tasks">📝 View All Tasks</a></li>
-      <li><a href="/new">➕ Add New Task</a></li>
-    </ul>
-  </nav>
+	<h1>Construction App</h1>
+	<nav>
+		<ul>
+			<li><a href="/tasks">📝 View All Tasks</a></li>
+			<li><a href="/new">➕ Add New Task</a></li>
+		</ul>
+	</nav>
 </main>
