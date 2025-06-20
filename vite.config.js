64ebe8b02fb5ce492fa5a9c5068d5eee350be785
Mariano <marianosgimenez@gmail.com>
2025-06-20 17:50:40 +0200

    update dependencies; remove 'pouchdb' install 'events', set vite.config.js to include global definition and alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+export default {
+  plugins: [sveltekit()],
+  define: {
+    global: 'window'
+  },
+  resolve: {
+    alias: {
+      events: 'events'
+    }
+  }
+};
